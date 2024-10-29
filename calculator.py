336f314812236af8cb24142a3627b0f0436d6f02
Jancee Rod <jancee800@gmail.com>
2024-10-29 16:51:10 +0800

    add get inputs from user AGAIN

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# hello hehe
+
+"""
+    initiate app - rico
+    get input from user - irish
+    do operations - jancee
+""" 
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
